256125db9b53f0394d1b013fe970ccea06c0faeb
jonathan <jagundezl@accem.es>
2021-04-10 03:51:26 +0200

    Functional router and persistent store

--- extension/index.js
+++ extension/index.js
@@ -3,23 +3,15 @@
 
 module.exports = function (nodecg) 
 {
-	var lastfm_config = require("./model/Config.js")(nodecg)
-	var api_key = lastfm_config.getApiKey();
-	var username = lastfm_config.getUsername();
-
-	nodecg.log.debug(api_key)
+	// var lastfm_config = require("./model/Config.js")(nodecg)
+	// var api_key = lastfm_config.getApiKey();
+	// var username = lastfm_config.getUsername();
+	// nodecg.log.debug(api_key)
 	
 	nodecg.Replicant('textReplicant', {defaultValue: 'TEST 1'})
 	
-	nodecg.Replicant('API_KEY', api_key)
-	nodecg.Replicant('USERNAME', username)
-
-	const usernameReplicant = nodecg.Replicant('USERNAME');
-	const apiKeyReplicant   = nodecg.Replicant('API_KEY');
-	usernameReplicant.on('change', value=> {
-		nodecg.log.debug("????")
-		lastfm_config.setUsername(value);
-	})
+	nodecg.Replicant('API_KEY', {defaultValue: "", persistent: true})
+	nodecg.Replicant('USERNAME', {defaultValue: "", persistent: true})
 	
 	require('./last-fm-api-ext')(nodecg);
 };
--- extension/last-fm-api-ext.js
+++ extension/last-fm-api-ext.js
@@ -3,10 +3,16 @@ module.exports = function (nodecg) {
     const { window } = new JSDOM( "" );
     const $ = require( "jquery" )( window );
     
-    var LASTFM_API_KEY       = nodecg.Replicant('API_KEY');
-    var LASTFM_USERNAME      = nodecg.Replicant('USERNAME');
+    var LASTFM_API_KEY       = nodecg.Replicant('API_KEY').value;
+    var LASTFM_USERNAME      = nodecg.Replicant('USERNAME').value;
     var IMAGE_LASTFM_NOIMAGE = null;                          // will be a resource from nodecg
 
+    const app = nodecg.Router();
+    app.get('/lastfm/get', (req, res) => {
+        makeRequest()
+        res.send();
+    });
+    nodecg.mount(app);
 
 	function load_lastfm_data() {
 
@@ -23,12 +29,11 @@ module.exports = function (nodecg) {
     {
         var url = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user="
         +LASTFM_USERNAME+"&api_key="+LASTFM_API_KEY+"&format=json";
-        nodecg.log.debug(url)
         $.getJSON(url, function(data) {
             nodecg.log.debug(url)
-            nodecg.Replicant('lastFmResponse', data)
+            nodecg.Replicant('lastFmResponse').value = data
         });
-
+        return
     }
     
     makeRequest()
